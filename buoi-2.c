20bd4a5322f273645b355c574255bd6669b95806
congly767 <lcong6088@gmail.com>
2025-11-24 21:35:49 +0700

    Add files via upload

---
+++ buoi-2.c
@@ -0,0 +1,54 @@
+// Comment nghia la khong cho trinh bien dich dich dong code nay
+// Day la tien xu ly (preprocesser). Muc dich la nhung thu vien
+// Standard Input Output (thu vien nhap xuat chuan)
+// vao trong file thuc thi
+#include <stdio.h>
+
+// Day la ham chuong trinh se thuc thi dau tien
+int main(){
+	// day la than ham
+	
+	// Ham printf (printf formatted) dung de in ra man hinh 1 cai gi do
+	printf("Xin Chao \n");
+	
+	int tuoi = 0;
+	// %d, %f, %lf, %c... goi la format specifier (dac ta dinh dang)
+	printf("Tuoi cua toi la: %d \n", tuoi);
+	
+	float diemToan = 4.5;
+	float diemVan = 7.5;
+	float diemAnh = 5.5;
+	float diemTrungBinh;
+	
+	printf("Diem toan: %f \n", diemToan);
+	printf("Diem van: %f \n", diemVan);
+	printf("Diem anh: %f \n", diemAnh);
+	
+	diemTrungBinh = (diemToan + diemVan + diemAnh) / 3;
+	
+	printf("Diem trung binh la %f \n", diemTrungBinh);
+	
+	char kyTu = 'A';
+	
+	printf("Gia tri cua bien kyTu la: %c \n", kyTu);
+	
+	// De nhap du lieu tu ban phim thi minh dung scanf()
+	int a;
+	int b;
+	int tong;
+	printf("Nhap so a: ");
+	// Dau & la toan tu dia chi, dung de xac dinh gia tri cua bien a
+	// duoc luu o dau trong bo nho RAM
+	scanf("%d", &a);
+	
+	printf("Gia tri cua bien a la: %d \n", a);
+	
+    printf("Nhap so b: ");
+    scanf("%d", &b);
+    printf("gia tri cua bien b la: %d \n", b);
+    
+    tong = a + b;
+    printf("Tong cua a va b la: %d", tong);
+    
+	return 0;
+}
